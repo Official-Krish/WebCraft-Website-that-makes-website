cd9e662eed5d794ffac41d64dc0d336e9814d6d2
Official-Krish <krishanand0103@gmail.com>
2024-12-29 00:54:51 +0530

    fixed zod validation

--- backend/src/types/userSchema.ts
+++ backend/src/types/userSchema.ts
@@ -1,12 +1,11 @@
 import z from 'zod';
 
 export const SignupSchema = z.object({
-    name: z.string().min(3),
-    email: z.string().min(5),
-    password: z.string().min(6),
+    email: z.string().min(5).email(),
+    password: z.string().min(8).regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/),
 });
 
 export const LoginSchema = z.object({
-    email: z.string().min(5),
-    password: z.string().min(8),
+    email: z.string().min(5).email(),
+    password: z.string().min(8).regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/),
 });
