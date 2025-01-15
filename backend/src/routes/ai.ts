fc1342daf50c07b79794061e51d3a28ae18e7274
Official-Krish <krishanand0103@gmail.com>
2025-01-15 23:51:48 +0530

    shifted to gemini

--- backend/src/routes/ai.ts
+++ backend/src/routes/ai.ts
@@ -1,27 +1,29 @@
 import { Router } from "express";
-import Anthropic  from "@anthropic-ai/sdk";
-import { TextBlock } from "@anthropic-ai/sdk/resources";
 import { BASE_PROMPT, getSystemPrompt } from "../utils/prompts";
 import { basePrompt as reactBasePrompt } from "../defaults/react-base";
 import { basePrompt as nodeBasePrompt } from "../defaults/node-base";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const aiRouter = Router();
 
-const anthropic = new Anthropic();
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY ?? "");
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" })
 
 aiRouter.post("/template", async (req, res) => {
-    const prompt = req.body.prompt;
+    const prompt = req.body.prompt + "Return either react or node based on what do you think this project should be. Only return a single word response, either react or node. Do not return anything else.";
     
-    const response = await anthropic.messages.create({
-        messages: [{
-            role: 'user', content: prompt
-        }],
-        max_tokens: 200,
-        model: "claude-3-5-sonnet-20241022",
-        system: "Return either react or node based on what do you think this project should be. Only return a single word response, either react or node. Do not return anything else."
-    });
+    // const response = await anthropic.messages.create({
+    //     messages: [{
+    //         role: 'user', content: prompt
+    //     }],
+    //     max_tokens: 200,
+    //     model: "claude-3-5-sonnet-20241022",
+    //     system: "Return either react or node based on what do you think this project should be. Only return a single word response, either react or node. Do not return anything else."
+    // });
+    const result = await model.generateContent(prompt);
     
-    const answer = (response.content[0] as TextBlock).text;
+    let answer = result.response.text();
+    answer = answer.trim();
 
     if (answer === "react") {
         res.status(200).json({ 
@@ -36,21 +38,20 @@ aiRouter.post("/template", async (req, res) => {
         });
         return;
     } else {
+        console.log("here");
         res.status(400).json({ message: "Invalid response" });
         return;
     }
 });
 
 aiRouter.post("/chat", async (req, res) => {
-    const prompt = req.body.prompt;
-    const response = await anthropic.messages.create({
-        messages: prompt,
-        model: 'claude-3-5-sonnet-20241022',
-        max_tokens: 8000,
-        system: getSystemPrompt()
-    })
+    const userPrompt = req.body.prompt || ""; // Ensure prompt is not undefined
+    const prompt = `${userPrompt} ${getSystemPrompt()}`.trim();
+    console.log(prompt)
+    const response = await model.generateContent(prompt);
+    console.log(response.response.text)
 
-    res.status(200).json({ message: (response.content[0] as TextBlock).text });
+    res.status(200).json({ message: response.response.text });
 });
 
 export default aiRouter;
