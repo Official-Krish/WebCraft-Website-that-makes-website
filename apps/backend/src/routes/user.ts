fe4189559d9a42ebbe2b4322f4422b6765721be0
Official-Krish <krishanand0103@gmail.com>
2025-03-14 01:40:22 +0530

    fix user endpoints

--- apps/backend/src/routes/user.ts
+++ apps/backend/src/routes/user.ts
@@ -4,7 +4,6 @@ import bcrypt from "bcrypt";
 import { prisma } from "../utils/db";
 import jwt from "jsonwebtoken";
 import { LoginSchema, SignupSchema } from "../types/userSchema";
-import axios from "axios";
 
 const userRouter: Router = Router();
 
@@ -14,18 +13,19 @@ userRouter.post("/signup", async ( req,res ) => {
     const parsedData = SignupSchema.safeParse(req.body);
 
     if (!parsedData.success) {
-        return res.status(400).json({ message: "Invalid request body" });
+        res.status(400).json({ message: "Invalid request body" });
+        return;
     }
 
     const { email, password, name } = parsedData.data;
 
     if (!email || !password) {
-        return res.status(400).json({ message: "Email and password are required" });
+        res.status(400).json({ message: "Email and password are required" });
     }
 
     const existingUser = await prisma.user.findFirst({ where: { email } });
     if (existingUser) {
-        return res.status(400).json({ message: "User already exists" });
+        res.status(400).json({ message: "User already exists" });
     }
 
     try {
@@ -51,24 +51,27 @@ userRouter.post("/login", async ( req,res ) => {
     const parsedData = LoginSchema.safeParse(req.body);
 
     if (!parsedData.success) {
-        return res.status(400).json({ message: "Invalid request body" });
+        res.status(400).json({ message: "Invalid request body" });
+        return;
     }
 
     const { email, password } = parsedData.data;
     if (!email || !password) {
-        return res.status(400).json({ message: "Email and password are required" });
+        res.status(400).json({ message: "Email and password are required" });
     }
     const user = await prisma.user.findUnique({ where: {
             email : email
         } 
     });
     if (!user) {
-        return res.status(401).json({ message: "Invalid email or password" });
+        res.status(401).json({ message: "Invalid email or password" });
+        return;
     }
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
-        return res.status(401).json({ message: "Invalid email or password" });
+        res.status(401).json({ message: "Invalid email or password" });
+        return;
     }
 
     const token = jwt.sign({ userId: user.id }, JWT_SECRET, { expiresIn: "1h" });
