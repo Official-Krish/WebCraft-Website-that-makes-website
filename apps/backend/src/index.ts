5c434a3beefd94bec81466be6de1d5b5f51e3dea
Official-Krish <krishanand0103@gmail.com>
2025-04-27 18:15:07 +0530

    [docker]: added .dockerignore

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import userRouter from './routes/user';
-import aiRouter from './routes/ai';
 import projectRouter from './routes/project';
 
 const app = express();
@@ -12,7 +11,6 @@ app.use(cors({
 }));
 
 app.use('/api/v1/user', userRouter);
-app.use("/api/v1/AI", aiRouter);
 app.use("/api/v1/project", projectRouter);
 
 app.listen(3000, () => {
