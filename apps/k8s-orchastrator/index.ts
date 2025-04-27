76bd927b07a11a452b79321a7af66b13658358fd
Official-Krish <krishanand0103@gmail.com>
2025-04-27 17:30:06 +0530

    [feat]: added k8s orchastrator

---
+++ apps/k8s-orchastrator/index.ts
@@ -0,0 +1,298 @@
+import express from 'express';
+import cors from 'cors';
+import prisma from "@repo/db/client"
+import { KubeConfig } from "@kubernetes/client-node";
+import * as k8s from "@kubernetes/client-node";
+import { Writable } from 'stream';
+import { DOMAIN } from './config';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const kc = new KubeConfig();
+kc.loadFromDefault();
+
+const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+const currentContext = kc.getCurrentContext();
+const cluster = kc.getCluster(currentContext); 
+const networkingApi = kc.makeApiClient(k8s.NetworkingV1Api);
+
+async function listPods() {
+    const res =  await k8sApi.listNamespacedPod({ namespace: "user-apps" });
+    return res.items.filter(pod => pod.status?.phase === "Running" || pod.status?.phase === "Pending").filter(pod => pod.metadata?.name).map(pod => pod.metadata?.name);
+}
+
+export async function createIngressForProject(projectId: string) {
+    const ingressManifest = {
+        metadata: {
+            name: `ingress-${projectId}`,
+            namespace: "user-apps",
+            annotations: {
+                "kubernetes.io/ingress.class": "nginx", 
+                "cert-manager.io/cluster-issuer": "letsencrypt-prod", 
+            },
+        },
+        spec: {
+            rules: [
+                {
+                    host: `session-${projectId}.${DOMAIN}`,
+                    http: {
+                        paths: [
+                            {
+                                path: "/",
+                                pathType: "Prefix",
+                                backend: {
+                                    service: {
+                                        name: `session-${projectId}`,
+                                        port: {
+                                            number: 8080,
+                                        },
+                                    },
+                                },
+                            },
+                        ],
+                    },
+                },
+                {
+                    host: `preview-${projectId}.${DOMAIN}`,
+                    http: {
+                        paths: [
+                            {
+                                path: "/",
+                                pathType: "Prefix",
+                                backend: {
+                                    service: {
+                                        name: `preview-${projectId}`,
+                                        port: {
+                                            number: 8080,
+                                        },
+                                    },
+                                },
+                            },
+                        ],
+                    },
+                },
+                {
+                    host: `worker-${projectId}.${DOMAIN}`,
+                    http: {
+                        paths: [
+                            {
+                                path: "/",
+                                pathType: "Prefix",
+                                backend: {
+                                    service: {
+                                        name: `worker-${projectId}`,
+                                        port: {
+                                            number: 8080,
+                                        },
+                                    },
+                                },
+                            },
+                        ],
+                    },
+                },
+            ],
+            tls: [
+                {
+                    hosts: [
+                        `session-${projectId}.${DOMAIN}`,
+                        `preview-${projectId}.${DOMAIN}`,
+                        `worker-${projectId}.${DOMAIN}`,
+                    ],
+                    secretName: `cert-${projectId}`, // TLS secret name
+                },
+            ],
+        },
+    };
+    try {
+        await networkingApi.createNamespacedIngress({ namespace: "user-apps", body: ingressManifest});
+        console.log(`Ingress created for project ${projectId}`);
+    } catch (error) {
+        console.error("Error creating ingress:", error);
+    }
+}
+
+async function createPod(name: string) {
+    await k8sApi.createNamespacedPod( { namespace: "user-apps", body: {
+        metadata: {
+            name: name,
+            labels: {
+                app: name
+            }
+        },
+        spec: {
+            containers: [{
+                name: "code-server",
+                image: "krishanand01/code-server:v1",
+                ports: [{
+                    containerPort: 8080
+                }, {
+                    containerPort: 8081
+                }],
+            }, {
+                name: "ws-relayer",
+                image: "krishanand01/ws-relayer:v1",
+                ports: [{
+                    containerPort: 9093
+                }],
+            }, {
+                name: "worker",
+                image: "krishanand01/worker:v1",
+                ports: [{
+                    containerPort: 4000
+                }],
+                env: [{
+                    name: "WS_RELAYER_URL",
+                    value: "ws://localhost:9093"
+                }, {
+                    name: "GEMINI_API_KEY",
+                    valueFrom: {
+                        secretKeyRef: {
+                            name: "worker-secret",
+                            key: "GEMINI_API_KEY"
+                        }
+                    }
+                }, {
+                    name: "DATABASE_URL",
+                    valueFrom: {
+                        secretKeyRef: {
+                            name: "worker-secret",
+                            key: "DATABASE_URL"
+                        }
+                    }
+                }]
+            }]
+        }
+    }});
+
+    await k8sApi.createNamespacedService( { namespace: "user-apps", body: {
+        metadata: {
+            name: `session-${name}`,
+        },
+        spec: {
+            selector: {
+                app: name
+            },
+            ports: [{
+                port: 8080,
+                targetPort: 8080,
+                protocol: "TCP",
+                name: "session"
+            }]
+        }
+    }})
+
+    await k8sApi.createNamespacedService( { namespace: "user-apps", body: {
+        metadata: {
+            name: `preview-${name}`,
+        },
+        spec: {
+            selector: {
+                app: name
+            },
+            ports: [{
+                port: 8080,
+                targetPort: 8081,
+                protocol: "TCP",
+                name: "preview"
+            }]
+        }
+    }})
+
+    await k8sApi.createNamespacedService( { namespace: "user-apps", body: {
+        metadata: {
+            name: `worker-${name}`,
+        },
+        spec: {
+            selector: {
+                app: name
+            },
+            ports: [{
+                port: 8080,
+                targetPort: 4000,
+                protocol: "TCP",
+                name: "worker"
+            }]
+        }
+    }})
+}
+
+async function checkPodIsReady(name: string) {
+    let attempts = 0;
+    while(true){
+        const pod = await k8sApi.readNamespacedPod( { namespace: "user-apps", name });
+        if (pod.status?.phase === "Running") {
+            return;
+        }
+        if(attempts > 10) {
+            throw new Error("Pod is not ready");
+        }
+        await new Promise(resolve => setTimeout(resolve, 3000));
+        attempts++;
+    }
+}
+
+async function assignPodToProject(projectId: string) {
+    const pods = await listPods();
+    const podExits = pods.find(pod => pod === projectId);
+    if (!podExits) {
+        await createPod(projectId);
+    }
+    await checkPodIsReady(projectId);
+    const exec = new k8s.Exec(kc);
+    let stdout = "";
+    let stderr = "";
+
+    exec.exec("user-apps",projectId, "code-server", ["/bin/sh", "-c", "mv /tmp/react-app/* /app"],
+        new Writable({
+            write: (chunk: Buffer, encoding: BufferEncoding, callback: () => void) => {
+                stdout += chunk;
+                callback();
+            }
+        }),
+        new Writable({
+            write: (chunk: Buffer, encoding: BufferEncoding, callback: () => void) => {
+                stderr += chunk;
+                callback();
+            }
+        }), 
+        null,
+        false, 
+        (status) => {
+            console.log(status);
+            console.log(stdout);
+            console.log(stderr);
+        }
+    )
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    console.log("Pod is ready");
+    console.log(stdout);
+    console.log(stderr);
+}
+
+app.get("/worker/:projectId", async (req, res) => {
+    const { projectId } = req.params;
+    const project = await prisma.project.findUnique({
+        where: {
+            id: projectId
+        },
+    });
+
+    if (!project) {
+        res.status(404).json({ message: "Project not found" });
+        return;
+    }
+
+    await assignPodToProject(projectId);
+    res.status(200).json({
+        sessionUrl: `https://session-${projectId}.${DOMAIN}`,
+        previewUrl: `https://preview-${projectId}.${DOMAIN}`,
+        workerUrl: `https://worker-${projectId}.${DOMAIN}`,
+    })
+});
+
+
+app.listen(7000, () => {
+    console.log('K8s Orchestrator is running on port 7000');
+});
