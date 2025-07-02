49a7dd2916b0d9a75629ad5dd59e805d9fb0f624
Official-Krish <krishanand0103@gmail.com>
2025-07-02 18:01:46 +0530

    [feat]: added google auth

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import userRouter from './routes/user';
 import projectRouter from './routes/project';
+import GoogleRouter from './routes/googleAuth';
 
 const app = express();
 app.use(express.json());
@@ -14,6 +15,7 @@ app.use(cors({
 
 app.use('/api/v1/user', userRouter);
 app.use("/api/v1/project", projectRouter);
+app.use("/auth/google", GoogleRouter);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
